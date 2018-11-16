91d3bf0313ece936f3c3409d4e85aecee01af24f
Raudo Moquete <sanchezz09@hotmail.com>
2018-11-16 14:26:42 -0500

    Api Libraries done

---
+++ Model/Personas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Personas
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+    }
+}
---
+++ Persistence/Prueba_API_Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class Prueba_API_Context :DbContext
+    {
+        public Prueba_API_Context(DbContextOptions<Prueba_API_Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Personas> Personas { get; set; }
+    }
+}
